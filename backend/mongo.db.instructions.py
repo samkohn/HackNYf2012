b751f61e76557455f191acbcbb8bad94cd88962a
Raymond E <raymond.ehlers+git@gmail.com>
2012-09-29 17:42:04 -0400

    Mongo DB instructions

---
+++ backend/mongo.db.instructions.py
@@ -0,0 +1,12 @@
+# Mongodb instructions
+# Most of these things can be named whatever you'd like
+connect = pymongo.Connection("chipper.bu.edu")
+db = connect.hackny
+db.authenticate("hackny","hackny")
+# can be anything: this makes a hackNYTest test
+collection = db.hackNYTest
+# example object
+test = {'test': 'test'}
+collection.insert(test)
+# shows back the test 
+collection.find_one()
