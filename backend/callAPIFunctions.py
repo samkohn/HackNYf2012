c6c69af68960dc4f49d6b964901083daecd9940b
Raymond E <raymond.ehlers+git@gmail.com>
2012-09-29 23:16:08 -0400

    Helper function for server to call the diferent api functions

---
+++ backend/callAPIFunctions.py
@@ -0,0 +1,15 @@
+
+
+def findAPIType(api, type, search):
+    if api == "etsy":
+        
+    
+    if api == "tumblr":
+        
+    
+    if api == "behance":
+        
+    
+    if api == "bitly":
+        
+    
