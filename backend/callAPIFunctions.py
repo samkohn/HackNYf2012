92a6f1a3391b2de8ba0dbe3f465428885aa2ffba
Raymond E <raymond.ehlers+git@gmail.com>
2012-09-30 04:53:57 -0400

    Tried to add temboo, but having issues
    Currently commented out, but etsy appears to be working

--- backend/callAPIFunctions.py
+++ backend/callAPIFunctions.py
@@ -1,3 +1,5 @@
+from temboo.Library.Twitter.Search.SearchFilter import SearchFilter
+from temboo.core.session import TembooSession
 import process
 import pymongo
 import math
@@ -11,6 +13,35 @@ def findAPIType(api, type, search):
         else:
             returnObject = compareSize(process.etsyAPICall(type, search))
             return returnObject
+    
+    # govtrack
+    # elif api == "govtrack":
+    	# choreo = None;
+    	# inputs = None;
+    	
+    	# if "role" in type:
+    		# choreo = Role(session)
+    		# inputs = choreo.new_input_set()
+
+    		# if "party" in type:
+    			# inputs.set_Party(search)
+    		# elif "state" in type:
+    			# inputs.set_State(search)
+    	# elif type == "person/?gender":
+    		# choreo = Person(session)
+    		# inputs = choreo.new_input_set()
+    		# inputs.set_Gender(search)
+		
+		# elif type == "vote_voter/?person":
+			# choreo = VoteAndVoter(session)
+			# inputs = choreo.new_input_set()
+			# inputs.set_Person(search)
+			
+   		# inputs.set_Limit(5000)
+   		# results = choreo.execute_with_results(inputs)
+   		# count = results['meta']['total_count']
+        # returnObject = compareSize(process.etsyAPICall("",""))
+        # return returnObject
     
     if api == "tumblr":
         return "tumblr is not implemented"
@@ -28,22 +59,29 @@ def compareSize(size):
     collection = db.references
     
     # Find order of magnitude of the number given to the function
-    sizeOOMExp = math.floor(math.log(size,10))
-    sizeLowerLimit = math.pow(10, sizeOOMExp)
-    
+    if size != 0:
+        sizeOOMExp = math.floor(math.log(size,10))
+        sizeLowerLimit = math.pow(10, sizeOOMExp)
+    else:
+        sizeLowerLimit = 0
+        
     # Determine which objects are the same order of magnitude
     matchedObjects = []
     for col in collection.find():
         compSize = col['comparisonValue']
-        print compSize
+        #print compSize
         if (compSize >= sizeLowerLimit) & (compSize <= sizeLowerLimit*10):
             matchedObjects = matchedObjects + [col]
         
-    print matchedObjects
-    print len(matchedObjects)
-    matchedObjectIndex = random.randrange(0,len(matchedObjects))
+    #print matchedObjects
+    #print len(matchedObjects)
+    if len(matchedObjects) != 0:
+        matchedObjectIndex = random.randrange(0,len(matchedObjects))
+        returnObject = matchedObjects[matchedObjectIndex]
+    else:
+        matchedObjectIndex = 5
+        returnObject = collection.find()[matchedObjectIndex]
     
-    returnObject = matchedObjects[matchedObjectIndex]
     returnObject['factor'] = float(size)/returnObject['comparisonValue']
     returnObject['value'] = size
     # print "Testing Division"
@@ -56,5 +94,5 @@ def compareSize(size):
     # print divVal
     
     del returnObject['_id']
-    #print returnObject
+    print returnObject
     return returnObject
