11dfeb1c1e762d296ce1781d8d2f4d1961c95ae5
Sam <therunner05@gmail.com>
2012-09-29 20:55:14 -0400

    Tested out displaying results

--- frontend/public/javascripts/queryetsy.js
+++ frontend/public/javascripts/queryetsy.js
@@ -1,7 +1,7 @@
 $('document').ready( function() {
   $('#submit').click( function() {
     var $form = $('#etsyquery');
-    $form.attr('action', 'http://ec2.rjehlers.com:8080/');
+    $form.attr('action', 'http://ec2.rjehlers.com:8080/query/');
     $form.attr('method', 'GET');
     $form.submit();
     console.log('submitted');
--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -7,6 +7,23 @@ exports.index = function(req, res){
   res.render('index', { title: 'Hey There!' });
 };
 
+exports.result = function(req, res){
+  
+  // Get the response parameters
+  var response = {};
+
+  response.title= 'Results';
+  response.api = req.query.api;
+  response.value = req.query.value;
+  response.factor = req.query.factor;
+  response.comparison = req.query.comparison;
+  response.comparisonValue = req.query.comparisonValue; 
+  response.imageLink = req.query.image;
+
+  res.render('result', response);
+
+};
+
 exports.query = {};
 
 exports.query.etsy = function(req, res){
