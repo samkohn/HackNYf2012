465f8235190eb837fe24b40e83bf55db52734b56
Watson W. Blair <bestudios.wb@gmail.com>
2012-09-29 23:15:38 -0400

    Process.py updated

---
+++ backend/process.py
@@ -0,0 +1,10 @@
+import urllib2
+import json
+import sys
+
+def concat_URL(x):
+    URL = "http://openapi.etsy.com/v2/private/listings/active?limit=1000000&offset=0&keywords=" + x + "&api_key=f0zll6x1q1bislf7w0dgodg4"
+    responce = urllib2.urlopen(URL)
+    doc = json.loads(responce.read())
+    return doc['count']
+
