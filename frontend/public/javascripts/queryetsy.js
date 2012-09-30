dee7ee1943f93c9efbb9f001f6d65e326779e874
Sam <therunner05@gmail.com>
2012-09-29 23:44:52 -0400

    Can now receive a response from EC2

--- frontend/public/javascripts/queryetsy.js
+++ frontend/public/javascripts/queryetsy.js
@@ -1,10 +1,30 @@
 $('document').ready( function() {
   $('#submit').click( function() {
     var $form = $('#etsyquery');
-    $form.attr('action', 'http://ec2.rjehlers.com:8080/query/');
+    $form.attr('action', '/sendquery');
     $form.attr('method', 'GET');
     $form.submit();
     console.log('submitted');
+    /*
+    var type = $('#type').val();
+    var api = $('#api').val();
+    var search = $('#search').val();
+
+
+
+    var url = 'http://ec2.rjehlers.com:8080/query/?';
+    url += 'api=' + api + '&';
+    url += 'type=' + type + '&';
+    url += 'search=' + search;
+
+    console.log('getting url = ' + url);
+    $.get(url, function(responseText) {
+      var response = JSON.parse(responseText);
+      console.log('responseText = ' + responseText);
+      console.log('JSON.stringify(response) = ' + JSON.stringify(response));
+    });
+    */
+
   });
 
 
--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -1,4 +1,4 @@
-
+var http = require('http');
 /*
  * GET home page.
  */
@@ -28,4 +28,29 @@ exports.query = {};
 
 exports.query.etsy = function(req, res){
   res.render('queryetsy', { title: 'Etsy', api: 'Etsy' });
+};
+
+exports.sendquery = function(req, res){
+  var type = req.query.type;
+  var api = req.query.api;
+  var search = req.query.search;
+
+  var path = '/query/?';
+  path += 'api=' + api + '&';
+  path += 'type=' + type + '&';
+  path += 'search=' + search;
+
+  var host = 'ec2.rjehlers.com';
+  var port = 8080;
+  
+  var options = {host : host, port : port, path : path};
+
+  http.get(options, function(response) {
+    response.on('data', function(data) {
+      var response = JSON.parse(data);
+      console.log('data = ' + data);
+      console.log('JSON.stringify(response) = ' + JSON.stringify(response));
+      res.render('queryetsy', { title: 'Etsy', api: 'Etsy' });
+    });
+  });
 };
